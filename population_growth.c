875df8503307c885228b46876fcdf41d389db658
RandyKdev <rkwalar@gmail.com>
2021-07-10 01:56:20 +0100

    population growth problem solved

--- population_growth.c
+++ population_growth.c
@@ -4,5 +4,29 @@
 #include<stdio.h>
 
 int main(void) {
+    int start_size;
+    do{
+        printf("Start size: ");
+        scanf("%d", &start_size);
+    }while(start_size < 9);
+    
+    int end_size;
+    do {
+        printf("End size: ");
+        scanf("%d", &end_size);
+    }while(end_size < start_size);
+
+    int number_of_years = 0;
+    int number_dead, number_born;
+    while(start_size < end_size) {
+        number_born = start_size / 3;
+        number_dead = start_size / 4;
+        start_size += number_born - number_dead;
+        number_of_years++;
+    } 
+
+    printf("Years: %d\n", number_of_years);
+    
+    return 0;
 
 }
