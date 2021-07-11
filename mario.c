2f01fdc5b421c0ee4bc61fc92cfea7c873f81dc8
RandyKdev <rkwalar@gmail.com>
2021-07-11 05:58:27 +0100

    mario problem completed

---
+++ mario.c
@@ -0,0 +1,21 @@
+// link to problem statement
+// https://cs50.harvard.edu/x/2021/psets/1/mario/less/
+
+#include<stdio.h>
+
+int main(void) {
+    int height;
+    do{
+        printf("Height: ");
+        scanf("%d", &height);
+    } while(height < 1 || height > 8);
+
+    for(int i = 0; i < height; i++) {
+        for (int j = 0; j < height; j++)
+        {
+           if(j < height - i - 1) printf(" ");
+           else printf("#");
+        }
+        printf("\n");
+    }
+}
