ac8de3a6319608e659df3c478196704953893be1
RandyKdev <rkwalar@gmail.com>
2021-07-11 06:12:33 +0100

    mario_2 completed

--- mario_2.c
+++ mario_2.c
@@ -4,7 +4,26 @@
 #include<stdio.h>
 
 int main(void) {
-s
+    int height;
+    do {
+        printf("Height: ");
+        scanf("%d", &height);
+    } while(height < 1 || height > 8);
 
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height; j++) {
+            if(j < height - i - 1) printf(" ");
+            else printf("#");
+        }
+
+        printf("  ");
+
+        for (int k = 0; k <= i; k++)
+            printf("#");
+
+        printf("\n");
+    }
+    
     return 0;
 }
