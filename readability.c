378fe3fcf36460f3b8d254eac71f5df5a016de9a
RandyKdev <rkwalar@gmail.com>
2021-07-14 21:55:25 +0100

    readability completed

--- readability.c
+++ readability.c
@@ -4,10 +4,10 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<ctype.h>
+#include<math.h>
 
 int main(void) {
-    int c, letters = 0, words = 0, sentences = 0;
-    // bool inWord, inSentence;
+    int c, letters = 0, words = 1, sentences = 0;
 
     while((c = getchar()) != '\n') {
         if(isalpha(c)) {
@@ -18,8 +18,16 @@ int main(void) {
             sentences++;
         }
     }
-    printf("%d\n", letters);
-printf("%d\n", words);
-printf("%d\n", sentences);
+
+    float lettersPer100Words = ((float)letters / (float) words) * 100.0;
+    float sentencesPer100Words = ((float)sentences / (float) words) * 100.0;
+
+    float index = (0.0588 * lettersPer100Words) - (0.296 * sentencesPer100Words) - 15.8;
+    index = (float)round(index);
+
+    if(index < 1) printf("Before Grade 1\n");
+    else if(index > 15) printf("Grade 16+\n");
+    else printf("Grade %.0f\n", index);
+
     return 0;
 }
