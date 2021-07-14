61968f5b711603976178c382dd54379d00246f75
RandyKdev <rkwalar@gmail.com>
2021-07-14 21:23:19 +0100

    readability progressed

--- readability.c
+++ readability.c
@@ -2,8 +2,24 @@
 // https://cs50.harvard.edu/x/2021/psets/2/readability/
 
 #include<stdio.h>
+#include<stdbool.h>
+#include<ctype.h>
 
 int main(void) {
+    int c, letters = 0, words = 0, sentences = 0;
+    // bool inWord, inSentence;
 
+    while((c = getchar()) != '\n') {
+        if(isalpha(c)) {
+            letters++;
+        } else if(c == ' ') {
+            words++;
+        } else if (c == '?' || c == '.' || c == '!') {
+            sentences++;
+        }
+    }
+    printf("%d\n", letters);
+printf("%d\n", words);
+printf("%d\n", sentences);
     return 0;
 }
