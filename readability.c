c35f9073d01aebcc1222c711e3668e7295e337c8
RandyKdev <rkwalar@gmail.com>
2021-07-15 20:40:51 +0100

    trying out github actions

--- readability.c
+++ readability.c
@@ -34,7 +34,7 @@ void test() {
         "When he was nearly thirteen, my brother Jem got his arm badly broken at the elbow. When it healed, and Jem's fears of never being able to play football were assuaged, he was seldom self-conscious about his injury. His left arm was somewhat shorter than his right, when he stood or walked, the back of his hand was at right angles to his body, his thumb parallel to his thigh.",
         "There are more things in Heaven and Earth, Horatio, than are dreamt of in your philosophy.",
         "It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him.",
-        "a large class of computational problems involve the determination of properties of graphs, digraphs, integers, arrays of integers, finite families of finite sets, boolean formulas and elements of other countable domains." 
+        "A large class of computational problems involve the determination of properties of graphs, digraphs, integers, arrays of integers, finite families of finite sets, boolean formulas and elements of other countable domains." 
     };
     
     const int solutions[] = {-9, 2, 3, 5, 7, 8, 8, 9 , 10, 18};
