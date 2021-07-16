e01095e17986ee9f5db03919f0037927f9774dc7
RandyKdev <rkwalar@gmail.com>
2021-07-16 22:15:52 +0100

    tests added to scrabble.c

--- scrabble.c
+++ scrabble.c
@@ -4,40 +4,50 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-
-int compute_score(char *word);
-
-int main(void) {
-    // Get input words from both players
-    char word1[46], word2[46];
-
-    printf("Player 1: ");
-    scanf("%s", word1);
-
-    printf("Player 2: ");
-    scanf("%s", word2);
-
-    // Score both words
-    int score1 = compute_score(word1);
-    int score2 = compute_score(word2);
-
-    if(score1 > score2) printf("Player 1 wins!\n");
-    else if(score1 < score2) printf("Player 2 wins!\n");
-    else printf("Tie!\n");
-
-    return 0;
-}
 
 int compute_score(char *word) {
     int i = 0, c, score = 0;
     while((c = word[i++]) != '\0') {
         if(isupper(c))
-            score += POINTS[c - 65];
+            score += POINTS[c - 'A'];
         else if(islower(c))
-            score += POINTS[c - 97];
+            score += POINTS[c - 'a'];
     }
     return score;
 }
+
+void test() {
+    char *player1[] = {
+        "Question?",
+        "Oh,",
+        "COMPUTER",
+        "Scrabble"
+    };
+    char *player2[] = {
+        "Question!",
+        "hai!",
+        "science",
+        "wiNNeR"
+    };
+
+    char solution[] = {0, -1, 3, 5};
+
+    int score1, score2;
+    for(int i = 0; i < 4; i++) {
+        score1 = compute_score(player1[i]);
+        score2 = compute_score(player2[i]);
+        assert(score1 - score2 == solution[i]);
+    }
+}
+
+int main(void) {
+    
+    test();
+
+    return 0;
+}
+
