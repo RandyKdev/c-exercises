1a21465ea8572b89daeb0bb984ef261b7ba749dc
RandyKdev <rkwalar@gmail.com>
2021-07-19 06:56:59 +0100

    substitution.c started

---
+++ substitution.c
@@ -0,0 +1,10 @@
+// link to problem statement
+// https://cs50.harvard.edu/x/2021/psets/2/substitution/
+
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
