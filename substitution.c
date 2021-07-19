5a3ae35a214d06d77356a3446d4a82549f943cea
RandyKdev <rkwalar@gmail.com>
2021-07-19 07:45:19 +0100

    substitution done

--- substitution.c
+++ substitution.c
@@ -3,32 +3,51 @@
 
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
+
+#define LEN_OF_PLAINTEXT 50
+
+char getCipherChar(char plainChar, char *key);
 
 int main(int argc, char *argv[]) {
     if(argc != 2) {
         printf("Usage: ./substitution key\n");
         return 1;
     }
-    int len = strlen(argv[1]);
-    if(len != 26) {
-        printf("Key must contain 26 characters.");
+    int lenOfKey = strlen(argv[1]);
+    if(lenOfKey != 26) {
+        printf("Key must contain 26 characters.\n");
         return 1;
     }
-    for(int i = 0; i < len; i++) {
+    for(int i = 0; i < lenOfKey; i++) {
         if(!isalpha(argv[1][i])){
             printf("Key must contain only letters of the alphabet\n");
             return 1;
         }
     }
 
-    for(int i = 0; i < len; i++)
-        for(int j = i + 1; j < len; j++)
+    for(int i = 0; i < lenOfKey; i++)
+        for(int j = i + 1; j < lenOfKey; j++)
             if(argv[1][i] == argv[1][j]) {
                 printf("Key must contain unique letters\n");
                 return 1;
             }
 
+    char plaintext[LEN_OF_PLAINTEXT];
+    printf("plaintext: ");
+    scanf("%[^\n]s", plaintext);
+    printf("ciphertext: ");
+
+    for(int i = 0, lenOfPlaintext; i < strlen(plaintext); i++)
+        if(isalpha(plaintext[i])) printf("%c", getCipherChar(plaintext[i], argv[1]));
+        else printf("%c", plaintext[i]);
     
+    printf("\n");
 
     return 0;
+}
+
+char getCipherChar(char plainChar, char *key) {
+    int index = tolower(plainChar) - 'a';
+    return islower(plainChar) ? tolower(key[index]) : toupper(key[index]);
 }
