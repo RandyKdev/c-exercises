da4f1d234e315a546e2c5e0b8f2f6cf3afcf2a57
RandyKdev <rkwalar@gmail.com>
2021-07-20 22:41:33 +0100

    a pair of brackets removed

--- bubble_sort.c
+++ bubble_sort.c
@@ -11,14 +11,14 @@
 void bubbleSort(int *numbers, int length) {
     bool swapped = false;
     for(int i = 0; i < length; i++) {
-        for(int j = 0; j < length - 1; j++) {
+        for(int j = 0; j < length - 1; j++)
             if(numbers[j] > numbers[j + 1]) {
                 int temp = numbers[j];
                 numbers[j] = numbers[j + 1];
                 numbers[j + 1] = temp;
                 swapped = true;
             }
-        }
+        
         if(!swapped) return;
         swapped = false;
     }
@@ -44,7 +44,7 @@ void test() {
 }
 
 int main(void) {
-    
+
     test();
 
     return 0;
