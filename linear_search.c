9bc8db9f5564ca1f66a862d27b4ca0623332995c
RandyKdev <rkwalar@gmail.com>
2021-07-21 17:50:33 +0100

    linear search done

--- linear_search.c
+++ linear_search.c
@@ -6,13 +6,25 @@
 */
 
 #include<stdio.h>
+#include<assert.h>
 
-int linearSearch(int length, int* numbers) {
+int linearSearch(int length, int* numbers, int searchElem) {
+    for(int i = 0; i < length; i++) 
+        if(numbers[i] == searchElem) return i;
 
+    return -1;
 }
 
 void test() {
-    linearSearch();
+    int numbers1[] = {0, 5, 6, 2, -4};
+    int numbers2[] = {-3, -5, -7, -2};
+    int numbers3[] = {4, 5, 5, 6};
+    int numbers4[] = {10};
+
+    assert(linearSearch(5, numbers1, 0) == 0);
+    assert(linearSearch(4, numbers2, -7) == 2);
+    assert(linearSearch(5, numbers3, 5) == 1);
+    assert(linearSearch(1, numbers4, 4) == -1);
 }
 int main() {
 
