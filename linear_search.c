892b4d4d36c8f1a0150239cb8a16ef222d6aaafe
RandyKdev <rkwalar@gmail.com>
2021-07-21 17:43:39 +0100

    linear search started

---
+++ linear_search.c
@@ -0,0 +1,22 @@
+/**
+*@file
+*@brief A program that implements linear search
+*@details The program goes through a list of numbers (sorted or unsorted)
+* and returns the index of the first occurence of the search element if found, otherwise it returns -1
+*/
+
+#include<stdio.h>
+
+int linearSearch(int length, int* numbers) {
+
+}
+
+void test() {
+    linearSearch();
+}
+int main() {
+
+    test();
+
+    return 0;
+}
