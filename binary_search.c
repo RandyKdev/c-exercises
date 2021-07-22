03902dd62466ee7f75f4e51f60c31dc81f866c3f
RandyKdev <rkwalar@gmail.com>
2021-07-22 08:37:01 +0100

    binary search function done

--- binary_search.c
+++ binary_search.c
@@ -6,8 +6,17 @@
 #include<stdio.h>
 #include<assert.h>
 
-int binarySearch(int length, int* numbers) {
-    
+int binarySearch(int length, int* numbers, int searchElem) {
+    int start = 0;
+    int end = length - 1;
+    int middle;
+    while (start != end) {
+        middle = (start + end) / 2;
+        if(numbers[middle] == searchElem) return middle;
+        if(numbers[middle] > searchElem) end = middle - 1;
+        else start = middle + 1;
+    }
+    return -1;
 }
 
 void test() {
