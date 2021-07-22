b40de42e037bf68fdaf80fc08f68378760e9550f
RandyKdev <rkwalar@gmail.com>
2021-07-22 08:28:02 +0100

    binary search created

---
+++ binary_search.c
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * @brief A program to search for a number in a sorted list of numbers using the binary search algo
+ */
+
+#include<stdio.h>
+#include<assert.h>
+
+int binarySearch(int length, int* numbers) {
+    
+}
+
+void test() {
+
+}
+
+int main() {
+    
+    test();
+
+    return 0;
+}
