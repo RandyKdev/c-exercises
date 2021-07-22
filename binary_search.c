aea461f901941054822f8ac5d68a15ccb4674432
RandyKdev <rkwalar@gmail.com>
2021-07-22 08:59:36 +0100

    binary search tests added

--- binary_search.c
+++ binary_search.c
@@ -8,19 +8,35 @@
 
 int binarySearch(int length, int* numbers, int searchElem) {
     int start = 0;
-    int end = length - 1;
+    int end = length;
     int middle;
     while (start != end) {
         middle = (start + end) / 2;
         if(numbers[middle] == searchElem) return middle;
-        if(numbers[middle] > searchElem) end = middle - 1;
+        if(numbers[middle] > searchElem) end = middle;
         else start = middle + 1;
     }
     return -1;
 }
 
 void test() {
+    int numbers1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+    assert(binarySearch(10, numbers1, 2) == 1);
+    assert(binarySearch(10, numbers1, 6) == 5);
+    assert(binarySearch(10, numbers1, 9) == 8);
+    assert(binarySearch(10, numbers1, 10) == 9);
+    assert(binarySearch(10, numbers1, 1) == 0);
+    assert(binarySearch(10, numbers1, 11) == -1);
+
+    int numbers2[] = {-9, -8, -7, -6, -5, -4, -3, -2, -1, 0};
+
+    assert(binarySearch(10, numbers2, -9) == 0);
+    assert(binarySearch(10, numbers2, 0) == 9);
+    assert(binarySearch(10, numbers2, -5) == 4);
+    assert(binarySearch(10, numbers2, -3) == 6);
+    assert(binarySearch(10, numbers2, -7) == 2);
+    assert(binarySearch(10, numbers2, -10) == -1);
 }
 
 int main() {
