b3e8b733f2d09c1dd40b6cfb5df9dfe08b08fdd5
RandyKdev <rkwalar@gmail.com>
2021-07-23 23:46:35 +0100

    selection sort partly done

--- selection_sort.c
+++ selection_sort.c
@@ -7,11 +7,22 @@
 
 void selectionSort(int length, int* numbers) {
     int endOfSorted= 0; // exclusive
-    int startOfUnsorted = 0;
+    // int startOfUnsorted = 0; // [3, 2,1] -> [1, 2, 3] -> 1, 2, 3
+    int smallestIndex = 0;
+    int temp;
+    for(int i = 0; i < length - 1; i++) {
+        for(int j = endOfSorted; j < length; j++) {
+            if(numbers[j] < numbers[smallestIndex]) smallestIndex = j;
+        }
+        temp = numbers[smallestIndex];
+        numbers[smallestIndex] = numbers[endOfSorted];
+        numbers[endOfSorted] = temp;
+        endOfSorted++;
+    }
 }
 
 void test() {
-
+    
 }
 
 int main() {
