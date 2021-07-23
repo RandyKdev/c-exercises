3baf903cfbed7681bbf2e364bd815b9e4c38dc26
RandyKdev <rkwalar@gmail.com>
2021-07-23 22:43:46 +0100

    selection sort started

---
+++ selection_sort.c
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * @brief  A program to sort an array of numbers using the selection sort algo
+ */
+
+#include<stdio.h>
+
+void selectionSort(int length, int* numbers) {
+    int endOfSorted= 0; // exclusive
+    int startOfUnsorted = 0;
+}
+
+void test() {
+
+}
+
+int main() {
+
+    test();
+
+    return 0;
+}
