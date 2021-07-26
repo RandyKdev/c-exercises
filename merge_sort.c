9ed28f3d5134266638bfc4f6103874291ba1032d
Randy Kwalar <rkwalar@gmail.com>
2021-07-26 22:02:41 +0100

    a peek into merge sort

---
+++ merge_sort.c
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * @brief A program that implements merge sort algo
+ */
+
+#include<stdio.h>
+
+void merge_sort(int length, int *numbers) {
+    
+}
+
+void test() {
+
+}
+
+int main() {
+
+    test();
+
+    return 0;
+}
