0c79c913d4cc0e77b886ae669c2a1dd6e4e9ae93
Randy Kwalar <rkwalar@gmail.com>
2021-07-27 19:52:09 +0100

    tests added for merge sort

--- merge_sort.c
+++ merge_sort.c
@@ -4,13 +4,27 @@
  */
 
 #include<stdio.h>
+#include<assert.h>
 
-void merge_sort(int length, int *numbers) {
+void merge_sort(int numbers[], int leftIndex, int rightIndex) {
     
 }
 
 void test() {
+    int test1[] = {5,4,3,2,1};
+    int test2[] = {6,5,4,3,2,1};
+    int test3[] = {-2,-3,-1,0};
 
+    merge_sort(test1, 0, 4);
+    merge_sort(test2, 0, 5);
+    merge_sort(test3, 0, 3);
+
+    for(int i = 0; i < 5; i++) 
+        assert(test1[i] == i + 1);
+    for(int i = 0; i < 6; i++) 
+        assert(test2[i] == i + 1);
+    for(int i = 0; i < 4; i++)
+        assert(test3[i] == i - 3);    
 }
 
 int main() {
