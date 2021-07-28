de501998955030fc86b0e4a9ea52888ec458055f
Randy Kwalar <rkwalar@gmail.com>
2021-07-28 17:18:34 +0100

    documentation added

--- merge_sort.c
+++ merge_sort.c
@@ -3,33 +3,33 @@
  * @brief A program that implements merge sort algo
  */
 
-#include<stdio.h>
-#include<assert.h>
+#include<stdio.h>   /// for IO operations
+#include<assert.h>  /// for tests
 
 void merge_sort(int numbers[], int leftIndex, int rightIndex) {
 
     if(leftIndex == rightIndex) return; // edge case, when there is only one number in the array
 
-    int middle = (leftIndex + rightIndex) / 2; 
+    int middle = (leftIndex + rightIndex) / 2; // calculates the middle of array
 
     merge_sort(numbers, leftIndex, middle); // merge left sub array
     merge_sort(numbers, middle + 1, rightIndex); // merge right sub array
 
-    int tempLeftArr[middle - leftIndex + 1];
-    int tempRightArr[rightIndex - middle];
+    int tempLeftArr[middle - leftIndex + 1]; // left sub array
+    int tempRightArr[rightIndex - middle]; // right sub array
 
-    int tempLeftArrLength = 0;
-    int tempRightArrLength = 0;
+    int tempLeftArrLength = 0; // length of left sub array
+    int tempRightArrLength = 0; // length of right sub array
 
     // filling in left sub array
     for(int i = leftIndex; i <= middle; i++)
         tempLeftArr[tempLeftArrLength++] = numbers[i];
 
-    // filing in right sub array
+    // filling in right sub array
     for(int i = middle + 1; i <= rightIndex; i++)
         tempRightArr[tempRightArrLength++] = numbers[i];
 
-    // putting elements in left and right sub arrays in the main array sorted
+    // putting elements in left and right sub arrays in the main array, sorted
     for(int i = leftIndex, tempLeftArrIndex = 0, tempRightArrIndex = 0; i <= rightIndex; i++)
         if(tempLeftArrIndex == tempLeftArrLength) numbers[i] = tempRightArr[tempRightArrIndex++];
         else if(tempRightArrIndex == tempRightArrLength) numbers[i] = tempLeftArr[tempLeftArrIndex++];
