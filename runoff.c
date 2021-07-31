d6850f8fcda99c5e94d4f6209e6b33901699dd45
Randy Kwalar <rkwalar@gmail.com>
2021-07-31 22:54:31 +0100

    find_min function done

--- runoff.c
+++ runoff.c
@@ -171,8 +171,13 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    int min = 0;
+    for(int i = 0; i < candidate_count; i++) {
+        if(!candidates[i].eliminated && candidates[i].votes > min) {
+            min = candidates[i].votes;
+        }
+    }
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
