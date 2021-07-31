5a04e81b17fe34de8314245ad67a18cf0c5db869
Randy Kwalar <rkwalar@gmail.com>
2021-07-31 09:01:05 +0100

    print_winner function done

--- runoff.c
+++ runoff.c
@@ -159,7 +159,12 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    for(int i = 0; i < candidate_count; i++) {
+        if(!candidates[i].eliminated && candidates[i].votes >= voter_count / 2) {
+            printf("Winner: %s", candidates[i].name);
+            return true;
+        }
+    }
     return false;
 }
 
