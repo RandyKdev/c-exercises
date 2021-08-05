93758dc662435a4c367c28668913be9ea450871d
Randy Kwalar <rkwalar@gmail.com>
2021-08-05 18:30:50 +0100

    largest prime factor solution started

---
+++ Project_Euler/3_largest_prime_factor.c
@@ -0,0 +1,40 @@
+/**
+ * @file
+ * @brief Finds the largest prime factor of a given positive integer
+ * @details
+ * A brief overview
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * So given 13195 as input to the program it will return 29
+ * 600851475143
+ * link to [problem statement](https://projecteuler.net/problem=3)
+ */
+
+#include <assert.h> // for assert function
+#include <stdio.h>
+#include <inttypes.h> // for uint64_t, uint16_t data types
+
+/**
+ * @brief Finds the largest prime factor of `number`
+ * @param number the number to find the largest prime factor of
+ * @returns The largest prime factor of `number` 
+ */
+uint64_t largestPrimeFactor(uint64_t number) {
+
+}
+
+/**
+ * @brief Self-test Implementations
+ * @returns void
+ */
+void test() {
+    // The following lines tests the program of correct behaviour
+}
+
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+int main() {
+    test(); // runs self-test implementation of the program
+    return 0;
+}
