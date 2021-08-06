c061db480a67488e84c0b513d33054a631c00f8c
Randy Kwalar <rkwalar@gmail.com>
2021-08-06 18:37:34 +0100

    Project euler problem 4 started

---
+++ Project_Euler/4_largest_palindrome_product.c
@@ -0,0 +1,39 @@
+/**
+ * @file
+ * @brief Finds the largest palindrome product of 2 numbers having a certain number of digits
+ * @details
+ * A brief overview
+ * A palindromic number reads the same both ways. The largest palindrome 
+ * made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * link to [problem statement](https://projecteuler.net/problem=4)
+ */
+
+#include <assert.h>   // for assert function
+#include <inttypes.h> // for uint64_t, uint16_t data types
+
+/**
+ * @brief Calculates the largest palindrome product of 2 numbers having
+ * number of digits = `digits`
+ * @param digits the number of digits each, of the 2 numbers
+ * @returns the largest palindrome product
+ */
+uint64_t largetPalindromeProduct(uint64_t digits) {
+
+}
+
+/**
+ * @brief Self-test Implementations
+ * @returns void
+ */
+void test() {
+    // The following lines tests the program of correct behaviour
+}
+
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+int main() {
+    test(); // runs self-test implementation of the program
+    return 0;
+}
