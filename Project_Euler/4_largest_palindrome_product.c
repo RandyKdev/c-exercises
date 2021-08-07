ce28c50e4c9d0372d58f3f0adcf6fab5f8b2d74b
Randy Kwalar <rkwalar@gmail.com>
2021-08-07 22:31:49 +0100

    largest palinndrome done

--- Project_Euler/4_largest_palindrome_product.c
+++ Project_Euler/4_largest_palindrome_product.c
@@ -10,6 +10,44 @@
 
 #include <assert.h>   // for assert function
 #include <inttypes.h> // for uint64_t, uint16_t data types
+#include <math.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+uint8_t getDigitAtLocation(uint64_t number, uint32_t location) {
+    if(location == 0) return number % 10;
+    return (int)((number % (int)pow(10, location + 1)) - (number % (int)pow(10, location))) / (int)pow(10, location);
+}
+
+bool isPalindrome(uint64_t number) {
+    uint64_t length = 1;
+    uint64_t tempNum = number;
+    while((int)(tempNum / 10) > 0) {
+        length++;
+        tempNum = (int) (tempNum / 10);
+    }
+
+    for(uint64_t i = 0; i < length / 2; i++) {
+        if(getDigitAtLocation(number, i) != getDigitAtLocation(number, length - i - 1)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+uint64_t getSmallestNum(uint64_t digits) {
+    return pow(10, digits - 1);
+}
+
+uint64_t getLargestNum(uint64_t digits) {
+    uint64_t largestNum = 0;
+
+    for(int i = 0; i < digits; i++) {
+        largestNum += (9 * pow(10, i));
+    }
+
+    return largestNum;
+}
 
 /**
  * @brief Calculates the largest palindrome product of 2 numbers having
@@ -17,8 +55,22 @@
  * @param digits the number of digits each, of the 2 numbers
  * @returns the largest palindrome product
  */
-uint64_t largetPalindromeProduct(uint64_t digits) {
+uint64_t getLargetPalindromeProduct(uint64_t digits) {
+    uint64_t largestNum = getLargestNum(digits);
+    uint64_t smallestNum = getSmallestNum(digits);
+    uint64_t largestPalindrome = 0;
+    uint64_t temp;
 
+    for(uint64_t i = largestNum; i >= smallestNum; i--) {
+        for(uint64_t j = i; j >= smallestNum; j--) {
+            temp = i * j;
+            if(temp > largestPalindrome && isPalindrome(i * j)) {
+                largestPalindrome = i * j;
+            }
+        }
+    }
+    return largestPalindrome;
+    // printf("%ld\n", largestProduct);
 }
 
 /**
@@ -27,6 +79,9 @@ uint64_t largetPalindromeProduct(uint64_t digits) {
  */
 void test() {
     // The following lines tests the program of correct behaviour
+    assert(getLargetPalindromeProduct(4) == 99000099);
+    assert(getLargetPalindromeProduct(3) == 906609);
+    assert(getLargetPalindromeProduct(2) == 9009);
 }
 
 /**
