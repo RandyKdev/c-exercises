0b41df26a9e87c977c530802965a07a404aa74ac
Randy Kwalar <rkwalar@gmail.com>
2021-08-07 23:19:25 +0100

    documentation added for largest palindrome product

--- Project_Euler/4_largest_palindrome_product.c
+++ Project_Euler/4_largest_palindrome_product.c
@@ -1,6 +1,7 @@
 /**
  * @file
- * @brief Finds the largest palindrome product of 2 numbers having a certain number of digits
+ * @brief Finds the largest palindrome product of 2 numbers having 
+ * a certain number of digits
  * @details
  * A brief overview
  * A palindromic number reads the same both ways. The largest palindrome 
@@ -9,23 +10,56 @@
  */
 
 #include <assert.h>   // for assert function
-#include <inttypes.h> // for uint64_t, uint16_t data types
-#include <math.h>
-#include <stdio.h>
-#include <stdbool.h>
+#include <inttypes.h> // for uint64_t, uint32_t, uint8_t data types
+#include <math.h>     // for pow function
+#include <stdbool.h>  // for bool data type
 
-uint8_t getDigitAtLocation(uint64_t number, uint32_t location) {
-    if(location == 0) return number % 10;
-    return (int)((number % (int)pow(10, location + 1)) - (number % (int)pow(10, location))) / (int)pow(10, location);
+/**
+ * @brief Gets the Section Of `number` From `location` to 0
+ * @param number the number to get section from
+ * @param location the location of the digit to start sectioning from
+ * @returns The section of `number` from `location` to 0
+ */
+uint64_t getSectionOfNumberFromLocation(uint64_t number, uint32_t location) {
+    return number % (int)pow(10, location);
 }
 
+/**
+ * @brief Gets the Digit At `location` in `number` 
+ * @param number the number to get digit from
+ * @param location the location of the digit in `number`
+ * @returns The digit at `location` in `number`
+ */
+uint8_t getDigitAtLocation(uint64_t number, uint32_t location) {
+    // Check if `location` corresponds to the first digit of the number
+    if(location == 0) return number % 10;
+
+    return (int)(
+        getSectionOfNumberFromLocation(number, location + 1) - 
+        getSectionOfNumberFromLocation(number, location)
+    ) / 
+    (int) pow(10, location);
+}
+
+/**
+ * @brief Gets the Length Of `number` (the number of digits in `number`) 
+ * @param number the number to get its length
+ * @returns the length of `number`
+ */
+uint32_t getLengthOfNumber(uint64_t number) {
+    uint32_t length = 1; // 
+    while((int)(number /= 10) > 0 && length++);
+    return length;
+}
+
+/**
+ * @brief Checks if `number` is palindrome or not
+ * @param number the number to be checked for 'palindromeness'
+ * @returns `true` if `number` IS palindrome 
+ * @returns `false` if `number` is NOT palindrome 
+ */
 bool isPalindrome(uint64_t number) {
-    uint64_t length = 1;
-    uint64_t tempNum = number;
-    while((int)(tempNum / 10) > 0) {
-        length++;
-        tempNum = (int) (tempNum / 10);
-    }
+    uint64_t length = getLengthOfNumber(number);
 
     for(uint64_t i = 0; i < length / 2; i++) {
         if(getDigitAtLocation(number, i) != getDigitAtLocation(number, length - i - 1)) {
@@ -35,11 +69,23 @@ bool isPalindrome(uint64_t number) {
     return true;
 }
 
-uint64_t getSmallestNum(uint64_t digits) {
+/**
+ * @brief Gets the Smallest Number that can be represented in the 
+ * specified number of digits
+ * @param digits the number of digits the input number has
+ * @returns The smallest number with number of digits = `digits`
+ */
+uint64_t getSmallestNumber(uint64_t digits) {
     return pow(10, digits - 1);
 }
 
-uint64_t getLargestNum(uint64_t digits) {
+/**
+ * @brief Gets the Largest Number that can be represented in the 
+ * specified number of digits
+ * @param digits the number of digits the input number has
+ * @return The largest number with number of digits = `digits`
+ */
+uint64_t getLargestNumber(uint64_t digits) {
     uint64_t largestNum = 0;
 
     for(int i = 0; i < digits; i++) {
@@ -56,21 +102,31 @@ uint64_t getLargestNum(uint64_t digits) {
  * @returns the largest palindrome product
  */
 uint64_t getLargetPalindromeProduct(uint64_t digits) {
-    uint64_t largestNum = getLargestNum(digits);
-    uint64_t smallestNum = getSmallestNum(digits);
-    uint64_t largestPalindrome = 0;
-    uint64_t temp;
+    // Holds the largest number representable in the specified number of `digits`
+    uint64_t largestNum = getLargestNumber(digits); 
+    
+    // Holds the smallest number representable in the specified number of `digits`
+    uint64_t smallestNum = getSmallestNumber(digits);
 
+    // Holds the largest palindrome product of 2 numbers of with the specified number of `digits`
+    uint64_t largestPalindromeProduct = 0;
+
+    uint64_t temp; // holds the product of i and j temporarily
+
+    // 
     for(uint64_t i = largestNum; i >= smallestNum; i--) {
         for(uint64_t j = i; j >= smallestNum; j--) {
+
             temp = i * j;
-            if(temp > largestPalindrome && isPalindrome(i * j)) {
-                largestPalindrome = i * j;
+
+            // Checks if temp is greater than lPP and if temp is a palindrome number
+            if(temp > largestPalindromeProduct && isPalindrome(temp)) {
+                largestPalindromeProduct = temp; // sets largest palindrome product to temp
             }
         }
     }
-    return largestPalindrome;
-    // printf("%ld\n", largestProduct);
+
+    return largestPalindromeProduct;
 }
 
 /**
