afbb074008cd8b950a795be0cae9ba95dd092156
Randy Kwalar <rkwalar@gmail.com>
2021-08-09 19:54:41 +0100

    Add smallest multiple to Project Euler

---
+++ Project_Euler/5_smallest_multiple.c
@@ -0,0 +1,43 @@
+/**
+ * @file 
+ * @brief Gets the smallest possible multiple that is evenly divisible 
+ * by all integers in a certain continuous range
+ * @details
+ * A brief overview
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ * The program finds the smallest number that is evenly divisible by a continuous range of numbers
+ * link to [problem statement](https://projecteuler.net/problem=5)
+ */
+
+#include <stdio.h>
+#include <assert.h>
+#include <inttypes.h>
+
+/**
+ * @brief Gets the Smallest Possible Multiple that is evenly divisible 
+ * by all integers in the range `begin` to `end` inclusive
+ * @param begin marks the start of the range of numbers
+ * @param end marks the end of the range of numbers
+ * @returns The smallest possible multiple divisible by all numbers from `start` to `end`
+ */
+static uint64_t getSmallestMultiple(uint32_t begin, uint32_t end) {
+    return;
+}
+
+/**
+ * @brief Self-test Implementations
+ * @returns void
+ */
+static void test() {
+    // The following lines tests the program of correct behaviour
+    
+}
+
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+int main() {
+    test(); // runs self-test implementation of the program
+    return 0;
+}
