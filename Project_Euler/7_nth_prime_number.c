f7444eca5fa0e756640fe4147d67a6d8bae9ba8d
Randy Kwalar <rkwalar@gmail.com>
2021-08-11 18:56:50 +0100

    Add documentation to solution of problem 7 Project Euler

--- Project_Euler/7_nth_prime_number.c
+++ Project_Euler/7_nth_prime_number.c
@@ -14,9 +14,18 @@
 #include <stdbool.h>  // for bool data type
 #include <math.h>     // for for sqrt function
 
+/**
+ * @brief Checks if `number` is prime or not
+ * @param number the number to be checked if it is prime
+ * @returns `true` if `number` IS a prime number
+ * @returns `false` if `number` is NOT a prime number
+ */
 static bool isPrimeNumber(uint32_t number) {
+    // For loop starts from 3 because that is the next prime number after 2
+    // For loop ends at square root of `number` as in maths
+    // See solution to ``Problem 3`` for explanation
     for(uint32_t i = 3; i <= sqrt(number); i += 2) {
-        if(number % i == 0) {
+        if(number % i == 0) {  // checks if `number` is divisible by `i`
             return false;
         }
     }
@@ -24,17 +33,28 @@ static bool isPrimeNumber(uint32_t number) {
     return true;
 }
 
+/**
+ * @brief Gets the Prime Number at position = `position`
+ * @param position the position of the prime number
+ * @returns The prime number at position `position`
+ */
 static uint64_t getPrimeNumber(uint32_t position) {
+    // Checks if position is 1 and returns 2 
+    // which is the only prime number and the first prime number
     if(position == 1) return 2;
-    uint64_t primeNumber = 2;
-    uint32_t i = 3;
+
+    uint64_t primeNumber = 2;  // holds the most recent prime number gotten
+    uint32_t i = 3;  // counter variable
+    
+    // runs while `position` is greater than `1`
     while(position > 1) {
-        if(isPrimeNumber(i)) {
-            primeNumber = i;
-            position--;
+        if(isPrimeNumber(i)) { // checks if `i` is a prime number
+            primeNumber = i;  // sets `i` to `primeNumber`
+            position--;  // decrements `position`
         }
-        i += 2;
+        i += 2; // increments `i` by 2
     }
+
     return primeNumber;
 }
 
