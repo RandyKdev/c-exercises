423901c05a0e70e7399f52f47087817f2c1afff3
RandyKdev <rkwalar@gmail.com>
2021-08-15 14:58:32 +0100

    Modified calculator.c

--- calculator.c
+++ calculator.c
@@ -1,56 +1,107 @@
+/**
+ * @file
+ * @brief A simple calculator
+ */
+
 #include <stdio.h>
 
-int main() {
-    double a, b;
-    char operator;
-    printf("Enter first operand: ");
-    scanf("%lf", &a);
-    printf("Enter operator: ");
-    scanf(" %c", &operator);
-    printf("Enter second operand: ");
-    scanf("%lf", &b);
+void greet();
+void showOptions();
+int getOption();
+void getOperands(double *a, double *b);
+void printResult(int option, double a, double b);
 
-    if(operator == '+') {
-        printf("The sum of the two numbers is %lf\n", a + b);
-    } else if(operator == '-') {
-        printf("The difference of the two numbers is %lf\n", a - b);    
-    } else if(operator == '*') {
-        printf("The product of the two numbers is %lf\n", a * b);
-    } else if(operator == '/') {
-        if(b != 0) {
-            printf("The quotient of the two numbers is %lf\n", a / b);
-        } else {
-            printf("The divisor is equal to zero. ERROR!\n");
-        }
-    } else {
-        printf("Enter a valid mathematical operator\n");
+int main() {
+    greet(); // greets the user
+
+    int option; // holds the option the user will choose
+    double a, b; // a and b holds the 2 operands entered by the user
+
+    // An infinite while loop
+    while(1) {
+        showOptions(); // show various options for math operations
+        option = getOption(); // gets the user option
+        
+        // checks if the user decided to quit the program
+        if(option == 5) break;
+
+        getOperands(&a, &b); // gets the 2 operands from the user
+        printResult(option, a, b); // prints the result of the operation
+
+        // prints a divider
+        printf("_____________________________________________________________________\n\n");
     }
 
     return 0;
 }
 
-
-
-
-/*
-double add(double a, double b) {
-    return a + b;
+/**
+ * @brief Greets the user
+ * @returns void
+ */
+void greet() {
+    printf("Hello, I'm here to make your life easier\n\n");
 }
 
-double multiply(double a, double b) {
-    return a * b;
+/**
+ * @brief Shows the various options to the user
+ * @returns void
+ */
+void showOptions() {
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("5. Exit\n");
+    printf("\n");
 }
 
-double divide(double dividend, double divisor) {
-    return dividend / divisor;
+/**
+ * @brief Gets the user option
+ * @returns option choosen by user
+ */
+int getOption() {
+    int option;
+    do {
+        printf("Enter your choice (1-5): ");
+        scanf("%d", &option);
+    } while(option < 1 || option > 5);
+
+    return option;
 }
 
-double subtract(double subtrahend, double subtractor) {
-    return subtrahend - subtractor;
+/**
+ * @brief Gets the operands from the user
+ * @param a the first operand
+ * @param b the second operand
+ * @returns void
+ */
+void getOperands(double *a, double *b) {
+    printf("\nEnter any two numbers: ");
+    scanf("%lf %lf", a, b);
 }
 
- double sum = add(a, b);
-    double product = multiply(a, b);
-    double quotient = divide(a, b);
-    double difference = subtract(a, b);
-*/
+/**
+ * @brief Prints the result of the operation
+ * @param option option chosen by the user
+ * @param a the first operand
+ * @param b the second operand
+ * @returns void
+ */
+void printResult(int option, double a, double b) {
+    printf("\nResult: ");
+
+    if(option == 1) {
+        printf("%lf\n", a + b);
+    } else if(option == 2) {
+        printf("%lf\n", a - b);
+    } else if(option == 3) {
+        printf("%lf\n", a * b);
+    } else {
+        if(b != 0) {
+            printf("%lf\n", a / b);
+        } else {
+            printf("ERROR, divisor is zero (0)\n");
+        }
+    }
+}
