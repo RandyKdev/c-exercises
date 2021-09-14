4fcb543576153a9f92448fd56c7a52544114a64f
Neba-Emmanuel <nebaemmanuel736@gmail.com>
2021-09-14 19:49:21 +0100

    newCommits

---
+++ swap_two_nums.c
@@ -0,0 +1,14 @@
+A program to swap two numbers using temporary/third variable.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int x = 10, y = 15, temp;
+    temp = x;
+    x = y;
+    y = temp;
+    printf("x = %d and y = %d", x, y);
+    getch();
+}
