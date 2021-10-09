f9429156e4f19f75d0477355a86b2bbe1ae40936
Randy Kwalar <rkwalar@gmail.com>
2021-10-09 21:46:53 +0100

    Add documentation to bubble sort in DSA

--- DSA/bubble_sort.c
+++ DSA/bubble_sort.c
@@ -4,16 +4,27 @@
 *in ascending order using bubble sort algo
 */
 
-#include<stdio.h>
-#include<stdbool.h>
-#include<assert.h>
+#include <stdbool.h> // for bool data type
+#include <assert.h> // for assert function
 
+/**
+ * @brief Swaps numbers contained in `num1` and `num2`
+ * @param num1 Pointer to number to swap
+ * @param num2 Pointer to number to swap
+ * @returns void
+ */
 void swap(int* num1, int* num2) {
     int temp = *num1;
     *num1 = *num2;
     *num2 = temp;
 }
 
+/**
+ * @brief Sorts a list of numbers
+ * @param numbers the array of numbers to be sorted
+ * @param length the length of `numbers`
+ * @returns void
+ */
 void bubbleSort(int *numbers, int length) {
     bool swapped = false;
     for(int i = 0; i < length; i++) {
@@ -28,7 +39,12 @@ void bubbleSort(int *numbers, int length) {
     }
 }
 
+/**
+ * @brief Self-test Implementations
+ * @returns void
+ */
 void test() {
+     // The following lines tests the program of correct behaviour
     int numbers1[] = {1};
     int numbers2[] = {5, 4, 3, 2, 1, 0};
     int numbers3[] = {-1, -4, -2, 0};
@@ -47,9 +63,11 @@ void test() {
         assert(numbers3[i] == solution3[i]);
 }
 
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
 int main(void) {
-
-    test();
-
+    test(); // runs self-test implementation of the program
     return 0;
 }
