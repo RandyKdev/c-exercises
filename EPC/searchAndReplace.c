96e1b059f01a2780ad1c4bcf1b0f73c300440a02
Randy Kwalar <rkwalar@gmail.com>
2021-10-13 23:27:39 +0100

    Fix bug in search and replace in EPC

--- EPC/searchAndReplace.c
+++ EPC/searchAndReplace.c
@@ -1,3 +1,6 @@
+// For simplicity this solutions takes into condideration only 
+// the case where the search and replace strings are of equal length
+
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
@@ -9,19 +12,19 @@ char *searchAndReplace(const char *str, const char *search, const char *replace)
     char *newStrPtr = newStr;
     while(*(str + strlen(search) - 1)) {
         if(strncmp(str, search, strlen(search)) == 0) {
-            strncat(newStr, replace, strlen(replace));
+            strcat(newStr, replace);
             str += strlen(replace);
             newStr += strlen(replace);
             continue;
         }
         strncat(newStr++, str++, 1);
     }
-    *newStr = '\0';
+    strcat(newStr, str);
     return newStrPtr;
 }
 
 int main() {
-    printf("%s\n", searchAndReplace("hey i am a", "am", "he"));
+    printf("%s\n", searchAndReplace("hey i am", "am", "he"));
     // assert(strcmp(searchAndReplace("hey i am", "am", "hey"), "hey i hey") == 0);
     return 0;
 }
