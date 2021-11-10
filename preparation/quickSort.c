a4e0608074eca548447336fb3cc0bbd441f0d634
Randy Kwalar <rkwalar@gmail.com>
2021-11-10 04:45:27 +0100

    Add quickSort

--- preparation/quickSort.c
+++ preparation/quickSort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
 void swap(int *num1, int *num2) {
     int temp = *num1;
@@ -27,6 +28,12 @@ void quickSort(int arr[], int start, int end) {
     }
 }
 
+int compare(const void *p, const void *q) {
+    int p1 = *(int *) p;
+    int q1 = *(int *) q;
+    return p1 - q1;
+}
+
 int main() {
     int arr[] = {2,6,4,3,6,8};
     quickSort(arr, 0, 5);
@@ -36,5 +43,15 @@ int main() {
     assert(arr[3] == 6);
     assert(arr[4] == 6);
     assert(arr[5] == 8);
+
+
+    int arr1[] = {2,6,4,3,6,8};
+    qsort((void *)arr1, 6, sizeof(int), compare);
+    assert(arr1[0] == 2);
+    assert(arr1[1] == 3);
+    assert(arr1[2] == 4);
+    assert(arr1[3] == 6);
+    assert(arr1[4] == 6);
+    assert(arr1[5] == 8);
     return 0;
 }
